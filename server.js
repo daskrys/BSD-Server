49f5c67753c0642d279916db7bd410625c091dbf
daskrys <caperezl@ucsc.edu>
2024-02-13 20:11:51 -0800

    server now sends player nums

--- server.js
+++ server.js
@@ -11,35 +11,44 @@ const client_url = "https://daskrys.github.io/Socket-Prototype/";
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5174" || client_url,
+    //origin: "http://localhost:5174" || client_url, // for local development
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
-let connected_users = 0;
-let id;
+
+let next_player_num = 1;
+const players = [];
 
 io.on("connection", (socket) => {
-  connected_users++;
+  
+  const player_number = next_player_num;
+  players[socket.id] = player_number;
+  
+  // assign new player of their number
+  socket.emit('player_number', player_number);
   
   console.log("new user connected");
   
-  io.emit('users_connected', connected_users);
+  // update all clients
+  io.emit('users_connected', Object.keys(players).length);
   
-  id = socket.id;
 
   socket.on("disconnect", () => {
     console.log("user disconnected")
-    connected_users--;
+    
+    delete players[socket.id];
+    
+    // update all clients
+    io.emit('users_connected', Object.keys(players).length);
   });
   
   // add event listeners
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Bite-Sized Server");
 });
 
 const port = 3000;
 server.listen(port, () => console.log("server running on: port " + port));
-
-// uncomment when using server, gonna deploy client on github pages
